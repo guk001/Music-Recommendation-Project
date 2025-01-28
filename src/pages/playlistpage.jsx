c20773215f51f62c4ed3f0ccc7867be91dea3c33
guk001 <guk001@ucsd.edu>
2025-01-28 14:34:47 -0800

    router added

---
+++ src/pages/playlistpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PlaylistPage() {
+  return (
+    <div>
+      <h1>Playlist Page</h1>
+      <p>여기에 플레이리스트 결과를 보여주는 UI를 넣으세요.</p>
+    </div>
+  );
+}
+
+export default PlaylistPage;
---
+++ src/pages/questionpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function QuestionPage() {
+  return (
+    <div>
+      <h1>Question Page</h1>
+      <p>여기에 퀴즈 질문 UI를 넣으세요.</p>
+    </div>
+  );
+}
+
+export default QuestionPage;

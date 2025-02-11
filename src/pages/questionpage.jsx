fedd905a3308512a28e76bf10555e998133c50c0
lovewooyoo <153961948+lovewooyoo@users.noreply.github.com>
2025-02-11 15:03:05 -0800

    질문

--- src/pages/questionpage.jsx
+++ src/pages/questionpage.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import './questionpage.css';
+
 
 function QuestionPage() {
   return (
-    <div>
-      <h1>Question Page</h1>
-      <p>여기에 퀴즈 질문 UI를 넣으세요.</p>
-    </div>
+    <body>
+        <div className="question-container">
+            <p className="question"><b>오늘 하루 기분을 색깔로 표현한다면?</b></p>
+        </div>
+        <div className="middle">
+            <div className="options-container">
+                <div className="option" ><div className="number"><b>1</b></div><div className="content">Quis autem vel eum iure reprehenderit.Quis autem vel eum iure reprehenderit.</div></div>
+                <div className="option" ><div className="number"><b>2</b></div><div className="content">Quis autem vel eum iure reprehenderit.Quis autem vel eum iure reprehenderit.</div></div>
+                <div className="option" ><div className="number"><b>3</b></div><div className="content">Quis autem vel eum iure reprehenderit.Quis autem vel eum iure reprehenderit.</div></div>
+                <div className="option" ><div className="number"><b>4</b></div><div className="content">Quis autem vel eum iure reprehenderit.Quis autem vel eum iure reprehenderit.</div></div>
+            </div>
+        </div>
+    </body>
+    
   );
 }
 
